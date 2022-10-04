da448e475e557ad3ed6ed3766a4ef7adef15e927
Aziz <aziznurulloh8@gmail.com>
2022-10-04 07:21:32 +0700

    First Commit

---
+++ index.js.js
@@ -0,0 +1,10 @@
+// 20 Method bawaan Javascript
+
+// Berikut adalah beberapa method untuk memanipulasi string
+
+// 1. chartAt()
+// Berfungsi untuk Menyalin karakter dari indeks tertentu pada string. contoh :
+var nama='Kelas Programmer'
+console.log(nama.charAt(0))
+// Outputnya adalah : K
+
